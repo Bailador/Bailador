67ca90e5b42960760faffff5946b5fca551b3b66
Martin Barth <martin@senfdax.de>
2017-06-06 22:51:21 +0200

    Implemented more for Bailador::Configuration, including Fallbacks and user-defined values

--- lib/Bailador/Configuration.pm
+++ lib/Bailador/Configuration.pm
@@ -1,11 +1,18 @@
 use v6;
 
 class Bailador::Configuration {
+    ## USER DEFINED STUFF
+    has %.user-defined-suff;
+
     ## GENERAL STUFF
     has Str $.mode is rw where "production"|"development" = "production";
     has Str $.host is rw                                  = "127.0.0.1";
     has Int $.port is rw                                  = 3000;
     has Str $.layout is rw;
+
+    ## Commands
+    has Bool $.command-detection is rw = True;
+    has Str  $.default-command is rw;
 
     ## SESSION RELATED STUFF
     has Str $.cookie-name is rw       = 'bailador';
@@ -14,22 +21,44 @@ class Bailador::Configuration {
     has Str $.hmac-key is rw          = 'changeme';
     has Str $.backend is rw           = "Bailador::Sessions::Store::Memory";
 
+
+    method load-from-array(@args) {
+        for @*ARGS -> ($k, $v) { self.set($k, $v); }
+    }
     method load-from-env() {
         if %*ENV<BAILADOR> {
             my @pairs = %*ENV<BAILADOR>.split(',');
             for @pairs -> $p {
                 my ( $k, $v ) = $p . split(/<[:=]>/);
-                if $k eq 'mode' {
-                    $.mode = $v;
-                }
-                if $k eq 'port' {
-                    $.port = $v.Int;
-                }
-                if $k eq 'host' {
-                    $.host = $v;
-                }
+                set($k, $v);
             }
         }
+    }
 
+    multi method set($key, $value) {
+        if self.^can($key) {
+            self."$key"() = $value;
+        } else {
+            %.user-defined-stuff{$key} = $value;
+        }
+    }
+    multi method set(Pair $x) {
+        my $key = $x.key;
+        self.set($x.key, $x.value);
+    }
+
+    method get(Str $key) {
+        if self.^can($key) {
+            self."$key"(),
+        } else {
+            %.user-defined-stuff{$key};
+        }
+    }
+
+    multi method FALLBACK(Str $name) {
+        self.get($name);
+    }
+    multi method FALLBACK(Str $name, $value) {
+        self.set($name, $value);
     }
 }
