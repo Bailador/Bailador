191e92413713f8e66bc58c4f20f9e2fc506b62d5
Jonathan Scott Duff <duff@pobox.com>
2017-06-22 00:13:07 -0500

    Account for routes with no .path-str

--- lib/Bailador/Command/routes.pm
+++ lib/Bailador/Command/routes.pm
@@ -5,7 +5,7 @@ use Bailador::Command;
 class Bailador::Command::routes does Bailador::Command {
     method run(:$app) {
         for $app.routes -> $r {
-            say "$r.method.fmt("%10s")  {$r.path-str}";
+            put join " ", $r.method.fmt("%10s"), $r.path-str // $r.^name;
         }
     }
 }
