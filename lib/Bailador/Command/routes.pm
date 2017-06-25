c30097ac951cf305ec8f7ec5f363ae7ea4f6e406
Jonathan Scott Duff <duff@pobox.com>
2017-06-25 00:58:59 -0500

    update routes command to handle prefixes

--- lib/Bailador/Command/routes.pm
+++ lib/Bailador/Command/routes.pm
@@ -3,10 +3,28 @@ use v6;
 use Bailador::Command;
 
 class Bailador::Command::routes does Bailador::Command {
-    method run(:$app) {
-        for $app.routes -> $r {
-            my $path = $r.path-str // do { $r.^methods(:local).grep(*.name eq 'Str') && $r.Str } // $r.^name;
-            put join " ", $r.method.fmt("%10s"), $path;
+    sub print-routes($prefix,@routes) {
+        for @routes -> $r {
+            # Try to combine the prefix and .path-str
+            # If we don't have a .path-str, call .Str unless it was inherited from Mu
+            # otherwise just output the name of the Route class
+            my $path = $prefix ~ ($r.path-str //
+                                  do { $r.can('Str').[0].package.perl ne 'Mu' && $r.Str() } //
+                                  $r.^name);
+            if $r.routes > 0 { 
+                print-routes($path, $r.routes);
+            } else {
+                # because we've concatenated .perl strings, 
+                # get rid of the doubled "" in the middle
+                $path ~~ s:g/'""'//;    
+                for $r.method.list -> $method {
+                    put join " ", $method.fmt("%10s"), $path;
+                }
+            }
         }
+    }
+
+    method run(:$app) {
+        print-routes('',$app.routes);
     }
 }
