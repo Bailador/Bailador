738373be33ef4e5879e0a7a8467d540a8ea560de
Martin Barth <martin@senfdax.de>
2017-07-07 23:58:25 +0200

    routes get list output working again

--- lib/Bailador/Command/routes.pm
+++ lib/Bailador/Command/routes.pm
@@ -5,28 +5,29 @@ use Bailador::Route;
 
 class Bailador::Command::routes does Bailador::Command {
 
-    my sub output-plain($shortname, $method, $path, *@) {
-        put join " ", $shortname.fmt("%10s"), $method.fmt("%10s"), $path;
+    my sub output-plain($shortname, $method, @path, *@) {
+        put join " ", $shortname.fmt("%10s"), $method.fmt("%10s"), join " -> ", @path;
     }
 
-    my sub output-tree($shortname, $method, $path, @indent, @is-last) {
-        my @chars = map { ?$_ ?? "   " !! "┃  " }, @is-last;
+    my sub output-tree($shortname, $method, @path, @is-last) {
+        my @indent = map {' ' x $_.chars - 1}, @path;
+        my @chars  = map { ?$_ ?? "   " !! "┃  " }, @is-last;
         @chars[@chars.end] = @is-last[@is-last.end] ?? "┗━━" !! "┣━━" if +@is-last;
-        my $route = join "", (@indent Z~ @chars), $path;
+        my $route = join "", (@indent Z~ @chars), @path[ * - 1 ];
         put join " ", $shortname.fmt("%10s"), $method.fmt("%10s"), $route;
     }
 
-    my sub route-walker(Bailador::Route $r, &process, @indent, @is-last) {
-        @indent.push: ' ' x $r.route-spec.chars - 1;
-        process($r.^shortname, $r.method-spec, $r.route-spec, @indent, @is-last);
+    my sub route-walker(Bailador::Route $r, &process, @path, @is-last) {
+        @path.push: $r.route-spec;
+        process($r.^shortname, $r.method-spec, @path, @is-last);
         if $r.routes > 0 {
             for $r.routes.list.kv -> $i, $v {
                 @is-last.push: $i == +$r.routes.end;
-                route-walker($v, &process, @indent, @is-last);
+                route-walker($v, &process, @path, @is-last);
                 @is-last.pop;
             }
         }
-        @indent.pop;
+        @path.pop;
     }
 
     method run(:$app) {
