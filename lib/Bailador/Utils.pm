2d815be03927e1f973738d18b1baa25eb7312e05
Martin Barth <martin@senfdax.de>
2017-08-04 21:44:40 +0200

    color reset in Bailador::Utils::terminal-color

--- lib/Bailador/Utils.pm
+++ lib/Bailador/Utils.pm
@@ -9,7 +9,7 @@ unit module Bailador::Utils;
 # Colorize the terminal output with the given color, only if terminal-color setting is set to True.
 multi sub terminal-color(Str $text, Str $color, Bailador::Configuration $config) is export {
     if $config.terminal-color {
-        say color($color), $text;
+        say colored($text, $color);
     } else {
         say $text;
     }
